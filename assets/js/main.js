2fe8b1fa06dacda66813e0532a0bc8f0172f18f4
fdrbrs <federio.borsci@gmail.com>
2021-04-21 17:30:21 +0200

    javascript: alert and getElementById

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+//Questo è un alert
+
+alert('I am using Javascript');
+
+//Uso Javascript per cambiare dei testi dentro elementi con Id
+
+document.getElementById('playlist_title').innerHTML = 'This was changed with Javascript ❤';
+
+document.getElementById('crea_playlist').innerHTML = 'This too ❤';
+
+document.getElementById('upgrade_btn').innerHTML = 'UPGRADE TO JAVASCRIPT ❤';
+
+document.getElementById('artist').innerHTML = 'Javascript';
+
+document.getElementById('brano').innerHTML = 'Java';
+
+document.getElementById('nome').innerHTML = 'Script';
+
+document.getElementById('user').innerHTML = '❤ Javascript ❤';
